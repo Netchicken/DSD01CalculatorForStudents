58dc969c9485a920a72a807abd59a73af8fdb343
Gary <Gary.Dix@enform.nz>
2022-07-28 11:45:02 +1200

    Added Operations List

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 using System.ComponentModel.DataAnnotations;
 
@@ -17,6 +18,17 @@ namespace DSD01CalculatorForStudents.Pages
         [Display(Name = "Result")]
         public double Result { get; set; }
 
+        public string? OperationSelection { get; set; }
+
+        [Display(Name = "Operation")]
+        public List<SelectListItem> Operations = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "Add", Text = "Add" },
+            new SelectListItem { Value = "Subtract", Text = "Subtract" },
+            new SelectListItem { Value = "Multiply", Text = "Multiply" },
+            new SelectListItem { Value = "Divide", Text = "Divide" }
+        };
+
         public void OnGet()
         {
 
@@ -24,7 +36,26 @@ namespace DSD01CalculatorForStudents.Pages
 
         public void OnPost()
         {
-            Result = NumberA + NumberB;
+
+           
+
+            if (OperationSelection == "Add")
+                Result = NumberA + NumberB;
+
+            if (OperationSelection == "Subtract")
+            {
+                Result = NumberA - NumberB;
+            }
+            if (OperationSelection == "Multiply")
+            {
+                Result = NumberA * NumberB;
+            }
+            if (OperationSelection == "Divide")
+            {
+                Result = NumberA / NumberB;
+            }
+
+
         }
     }
 }
